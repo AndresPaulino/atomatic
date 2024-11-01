6e968a06320ec4336356a9154586c1b76e142a6b
Andres Paulino <paulinoandresmiguel@gmail.com>
2024-11-01 08:22:12 -0400

    Adjust header to remove search and messages components

--- components/partials/header/index.tsx
+++ components/partials/header/index.tsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import HeaderContent from './header-content'
-import HeaderSearch from './header-search'
-import ProfileInfo from './profile-info'
-import Notifications from './notifications'
-import Messages from "./messages"
-import ThemeSwitcher from './theme-switcher'
-import { SidebarToggle } from '@/components/partials/sidebar/sidebar-toggle'
-import { SheetMenu } from '@/components/partials/sidebar/menu/sheet-menu'
-import HorizontalMenu from "./horizontal-menu"
-import HeaderLogo from "./header-logo"
-
+import React from 'react';
+import HeaderContent from './header-content';
+import HeaderSearch from './header-search';
+import ProfileInfo from './profile-info';
+import Notifications from './notifications';
+import Messages from './messages';
+import ThemeSwitcher from './theme-switcher';
+import { SidebarToggle } from '@/components/partials/sidebar/sidebar-toggle';
+import { SheetMenu } from '@/components/partials/sidebar/menu/sheet-menu';
+import HorizontalMenu from './horizontal-menu';
+import HeaderLogo from './header-logo';
 
 const AtomaticHeader = async () => {
+  return (
+    <>
+      <HeaderContent>
+        <div className=' flex gap-3 items-center'>
+          <HeaderLogo />
+          <SidebarToggle />
+          {/* <HeaderSearch /> */}
+        </div>
+        <div className='nav-tools flex items-center  md:gap-4 gap-3'>
+          <ThemeSwitcher />
+          <Notifications />
+          <ProfileInfo />
+          <SheetMenu />
+        </div>
+      </HeaderContent>
+      <HorizontalMenu />
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <HeaderContent>
-                <div className=' flex gap-3 items-center'>
-                    <HeaderLogo />
-                    <SidebarToggle />
-                    <HeaderSearch />
-                </div>
-                <div className="nav-tools flex items-center  md:gap-4 gap-3">
-                    <ThemeSwitcher />
-                    <Messages />
-                    <Notifications />
-                    <ProfileInfo />
-                    <SheetMenu />
-                </div>
-            </HeaderContent>
-            <HorizontalMenu />
-        </>
-    )
-}
-
-export default AtomaticHeader
+export default AtomaticHeader;
